fbfee24abd45f388a7b87edf8384f9751333eccc
IvenaZhang <13253570397@163.com>
2026-04-20 00:27:31 +0800

    Add event function declarations in common.h

--- event/common.h
+++ event/common.h
@@ -1,1 +1,10 @@
+#ifndef EVENT_COMMON_H
+#define EVENT_COMMON_H
 
+#include "../player/player.h"
+
+void runBattleGodBlessingEvent(Player &player);
+void runJudgmentEvent(Player &player);
+void runMerchantEvent(Player &player);
+
+#endif
