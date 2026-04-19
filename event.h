48170a838f52336786940167e1aa0b4924be6423
IvenaZhang <13253570397@163.com>
2026-04-19 23:34:59 +0800

    Add event function declarations in event.h

--- event.h
+++ event.h
@@ -1,1 +1,10 @@
+#ifndef EVENT_H
+#define EVENT_H
 
+#include "../player/player.h"
+
+void runForestEvent(int eventId, Player &p);
+void runMountainEvent(int eventId, Player &p);
+void runAbyssEvent(int eventId, Player &p);
+
+#endif
