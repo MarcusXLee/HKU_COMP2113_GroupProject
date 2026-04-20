76506b7a7d05d7f3f4e461978a3cf1d11293ad48
IvenaZhang <13253570397@163.com>
2026-04-20 23:44:39 +0800

    Update event.h

--- event/event.h
+++ event/event.h
@@ -6,5 +6,7 @@
 void runForestEvent(int eventId, Player &p);
 void runMountainEvent(int eventId, Player &p);
 void runAbyssEvent(int eventId, Player &p);
+void runFoodCartEvent(Player &player);
+void runRevengeGodBlessingEvent(Player &player);
 
 #endif
