dcd6bcef36f6097efc5c36f4fafa123dba74087b
IvenaZhang <13253570397@163.com>
2026-04-21 00:27:26 +0800

    Update common.cpp

--- event/common.cpp
+++ event/common.cpp
@@ -19,9 +19,9 @@ void runBattleGodBlessingEvent(Player &player) {
         cout << "You kneel and pray for strength to defeat your enemies and avenge the Demon King." << endl;
         cout << "The God of Battle admires your courage and offers power in exchange for gold." << endl;
 
-        【function: changeGold(player, -20)】
-        【function: changeAttack(player, +1)】
-        【function: changeDefense(player, +1)】
+        changeGold(player, -20);//F
+        changeAttack(player, +1);//F
+        changeDefense(player, +1);//F
     }
     else if (choice == 2) {
         cout << "You believe the gods are only illusions." << endl;
@@ -44,14 +44,14 @@ void runJudgmentEvent(Player &player) {
     if (kindness >= 5) {
         cout << "Your kindness deserves a reward!" << endl;
 
-        【function: changeMaxHP(player, +5)】
-        【function: changeAttack(player, +1)】
-        【function: changeDefense(player, +1)】
+        changeMaxHP(player, +5);//F
+        changeAttack(player, +1);//F
+        changeDefense(player, +1);//F
     }
     else if (kindness > 0 && kindness < 5) {
         cout << "Your kindness is worth encouraging!" << endl;
 
-        【function: changeMaxHP(player, +3)】
+        changeMaxHP(player, +3);//F
     }
     else if (kindness < 0 && kindness > -5) {
         cout << "This is a warning!" << endl;
@@ -60,17 +60,17 @@ void runJudgmentEvent(Player &player) {
 
         if (r == 0) {
             cout << "You lost 1 Attack." << endl;
-            【function: changeAttack(player, -1)】
+            changeAttack(player, -1);//F
         }
         else {
             cout << "You lost 1 Defense." << endl;
-            【function: changeDefense(player, -1)】
+            changeDefense(player, -1);//F
         }
     }
     else if (kindness <= -5) {
         cout << "Your sins are unforgivable. Judgment must be carried out!" << endl;
 
-        【function: changeMaxHP(player, -5)】
+        changeMaxHP(player, -5);//F
     }
     else {
         cout << "Nothing happens." << endl;
@@ -104,18 +104,18 @@ void runMerchantEvent(Player &player) {
         cin >> subChoice;
 
         if (subChoice == 1) {
-            【function: changeGold(player, -5)】
-            【function: changeAttack(player, +1)】
+            changeGold(player, -5);//F
+            changeAttack(player, +1);//F
             cout << "You bought a cheap weapon." << endl;
         }
         else if (subChoice == 2) {
-            【function: changeGold(player, -15)】
-            【function: changeAttack(player, +2)】
+            changeGold(player, -15);//F
+            changeAttack(player, +2);//F
             cout << "You bought a fine weapon." << endl;
         }
         else if (subChoice == 3) {
-            【function: changeGold(player, -25)】
-            【function: changeAttack(player, +3)】
+            changeGold(player, -25);//F
+            changeAttack(player, +3);//F
             cout << "You bought a top weapon." << endl;
         }
     }
@@ -129,26 +129,26 @@ void runMerchantEvent(Player &player) {
         cin >> subChoice;
 
         if (subChoice == 1) {
-            【function: changeGold(player, -5)】
-            【function: changeDefense(player, +1)】
+            changeGold(player, -5);//F
+            changeDefense(player, +1);//F
             cout << "You bought a cheap armor." << endl;
         }
         else if (subChoice == 2) {
-            【function: changeGold(player, -15)】
-            【function: changeDefense(player, +2)】
+            changeGold(player, -15);//F
+            changeDefense(player, +2);//F
             cout << "You bought a fine armor." << endl;
         }
         else if (subChoice == 3) {
-            【function: changeGold(player, -25)】
-            【function: changeDefense(player, +3)】
+            changeGold(player, -25);//F
+            changeDefense(player, +3);//F
             cout << "You bought a top armor." << endl;
         }
     }
     else if (choice == 3) {
         cout << "You decide to rob the merchant!" << endl;
 
-        【function: enemy = createEnemy(merchant_id)】
-        【function: startBattle(player, enemy)】
+        Enemy enemy = createEnemy(merchant);//F and need modify
+        startBattle(player, enemy);
     }
 }
 
@@ -170,20 +170,20 @@ void runHuntingEvent(Player &player) {
     if (choice == 1) {
         cout << "You decide to hunt small prey." << endl;
 
-        【function: enemy = createEnemy(rabbit_id)】
-        【function: startBattle(player, enemy)】
+        Enemy enemy = createEnemy(rabbit_id);//F and need modify
+        startBattle(player, enemy);
     }
     else if (choice == 2) {
         cout << "You decide to hunt medium prey." << endl;
 
-        【function: enemy = createEnemy(dog_id)】
-        【function: startBattle(player, enemy)】
+        Enemy enemy = createEnemy(dog);////F and need modify
+        startBattle(player, enemy);
     }
     else if (choice == 3) {
         cout << "You decide to hunt large prey." << endl;
 
-        【function: enemy = createEnemy(bear_id)】
-        【function: startBattle(player, enemy)】
+        Enemy enemy = createEnemy(bear);//F and need modify
+        startBattle(player, enemy);//F
     }
 }
 
@@ -207,14 +207,14 @@ void runGamblingEvent(Player &player) {
 
         if (r < 25) {
             cout << "You lost 1 gold." << endl;
-            【function: changeGold(player, -1)】
+            changeGold(player, -1);//F
         }
         else if (r < 75) {
             cout << "Nothing happens." << endl;
         }
         else {
             cout << "You won 1 gold!" << endl;
-            【function: changeGold(player, +1)】
+            changeGold(player, +1);//F
         }
     }
 
@@ -223,14 +223,14 @@ void runGamblingEvent(Player &player) {
 
         if (r < 25) {
             cout << "You lost 5 gold." << endl;
-            【function: changeGold(player, -5)】
+            changeGold(player, -5);//F
         }
         else if (r < 75) {
             cout << "Nothing happens." << endl;
         }
         else {
             cout << "You won 5 gold!" << endl;
-            【function: changeGold(player, +5)】
+            changeGold(player, +5);//F
         }
     }
 
@@ -239,15 +239,103 @@ void runGamblingEvent(Player &player) {
 
         if (r < 25) {
             cout << "You lost all your gold." << endl;
-            【function: loseAllGold(player)】
+            loseAllGold(player);//F
         }
         else if (r < 75) {
             cout << "Nothing happens." << endl;
         }
         else {
             cout << "You doubled your gold!" << endl;
-            【function: doubleGold(player)】
+            doubleGold(player);//F
+        }
+    }   
+}
+    
+// Food Cart
+void runFoodCartEvent(Player &player) {
+
+    cout << "Food Cart" << endl;
+    cout << "The food cart arrives. Spend your gold to enjoy some perfect meals!" << endl;
+
+    cout << "1. Have a snack (-2 gold, restore 10% HP)" << endl;
+    cout << "2. Have a full meal (-5 gold, restore 30% HP)" << endl;
+    cout << "3. Try magical food (-15 gold)" << endl;
+
+    int choice;
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "You enjoy a small snack." << endl;
+
+        changeGold(player, -2);//F
+        healPlayer(player, getMaxHP(player) * 0.1);//F
+    }
+    else if (choice == 2) {
+        cout << "You enjoy a full meal." << endl;
+
+        changeGold(player, -5);//function
+        healPlayer(player, getMaxHP(player) * 0.3);//function
+    }
+    else if (choice == 3) {
+        cout << "You try the magical food..." << endl;
+
+        changeGold(player, -15);//F
+
+        int r = rand() % 2;
+
+        if (r == 0) {
+            cout << "You lost some Attack but gained great Defense!" << endl;
+
+            int loss = rand() % 4 + 1;  // 1~4
+
+            changeAttack(player, -loss);//function
+            changeDefense(player, 2 * loss);//function
+        }
+        else {
+            cout << "You lost some Defense but gained great Attack!" << endl;
+
+            int loss = rand() % 4 + 1;
+
+             changeDefense(player, -loss);//function
+             changeAttack(player, 2 * loss);//function
         }
     }
-
 }
+
+
+// Blessing of the God of Revenge
+void runRevengeGodBlessingEvent(Player &player) {
+
+    cout << "Blessing of the God of Revenge" << endl;
+    cout << "Guided by the God of Revenge, you arrive before the statue." << endl;
+    cout << "Your hatred toward the Demon King pleases the god." << endl;
+
+    cout << "1. Rational revenge" << endl;
+    cout << "2. Aggressive revenge" << endl;
+    cout << "3. Mad revenge" << endl;
+
+    int choice;
+    cin >> choice;
+
+    if (choice == 1) {
+        cout << "The God of Revenge is disappointed, but still grants you power." << endl;
+
+        changeAttack(player, +1);//F
+    }
+    else if (choice == 2) {
+        cout << "The god approves your determination and demands a price." << endl;
+
+        damagePlayer(player, 5);//F
+        changeAttack(player, +3);//F
+    }
+    else if (choice == 3) {
+        cout << "The God of Revenge is finally satisfied." << endl;
+        cout << "You give up everything for ultimate power." << endl;
+
+        setHPToOne(player);//F
+        changeAttack(player, -2);//F
+        doubleAttack(player);//F
+    }
+}
+
+
