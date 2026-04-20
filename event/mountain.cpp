d74c0a21df20e37d67e368529678f9659b1cfbcd
IvenaZhang <13253570397@163.com>
2026-04-21 01:00:30 +0800

    Enhance event handling with comments
    Added comments for event handling cases.

--- event/mountain.cpp
+++ event/mountain.cpp
@@ -2,9 +2,22 @@
 using namespace std;
 
 void runForestEvent(int eventId, Player &player) {
-    //event 1
+    
+//event 1
     else if (eventId == 1) {
         runBattleGodBlessingEvent(player);
     }
+
+//event 2      
+    else if (eventId == 2) {
+        runJudgmentEvent(player);
+    }
+
+        
+//event 3
+    else if (eventId == 3) {
+        runMerchantEvent(player);
+    }
+
 
 }
