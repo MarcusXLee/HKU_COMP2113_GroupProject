56a18e12a3b33d56e2d2ee62b2b20e3b672a1339
IvenaZhang <13253570397@163.com>
2026-04-22 00:56:59 +0800

    Remove FoodCart and RevengeGodBlessing event functions
    Remove unused event functions from event.h

--- event/event.h
+++ event/event.h
@@ -6,7 +6,5 @@
 void runForestEvent(int eventId, Player &p);
 void runMountainEvent(int eventId, Player &p);
 void runAbyssEvent(int eventId, Player &p);
-void runFoodCartEvent(Player &player);
-void runRevengeGodBlessingEvent(Player &player);
 
 #endif
